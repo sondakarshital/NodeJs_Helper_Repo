9ef4bafce6c1b29248a3c24e2aa5c2bcfb289855
sondakarshital <37493236+sondakarshital@users.noreply.github.com>
2021-05-23 11:11:54 +0530

    Add files via upload

--- NodeJs/node-i18n/index.js
+++ NodeJs/node-i18n/index.js
@@ -1,1 +1,27 @@
+var express = require('express')
+var url = require('url')
+var path = require('path')
+var i18n = require('i18n')
 
+i18n.configure({
+  locales: ['en', 'de', 'ar','kannada'],
+  directory: path.join(__dirname, 'locales'),
+  updateFiles: false
+})
+
+var app = express()
+app.use(i18n.init)
+
+app.get("/mylanguage/:lang",function(req,res){
+  console.log('req.params.lang ',req.params.lang)
+  i18n.setLocale(req, req.params.lang);
+  let msg = req.__('message');
+  let response =  req.__('Hello')+"!!! "+req.__('registration successful');
+  let message= {};
+  message[msg] = response;
+  res.send(message)
+})
+
+
+// startup
+app.listen(3000,()=>console.log("listening on 3000"))
