025d8c105677d121dfe485dedc5e41744f470fa2
Shital Shrikant Sondakar <Shital.Sondakar@ltts.com>
2024-05-14 18:40:59 +0530

    code update

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,18 +1,16 @@
+try {
+  const sgMail = require("@sendgrid/mail");
 
+  const sendgridAPIKey = "";
 
-try{
-const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(sendgridAPIKey);
 
-const sendgridAPIKey = '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'
-
-sgMail.setApiKey(sendgridAPIKey)
-
-sgMail.send({
-    to : 'sondakarshital@gmail.com',
-    from : 'sondakarshital@gmail.com',
-    subject : 'This is my first mail',
-    text : 'I hope you will get this'
-})
-}catch(e){
-    console.log("error ",e);
+  sgMail.send({
+    to: "s@gmail.com",
+    from: "s@gmail.com",
+    subject: "This is my first mail",
+    text: "I hope you will get this",
+  });
+} catch (e) {
+  console.log("error ", e);
 }
